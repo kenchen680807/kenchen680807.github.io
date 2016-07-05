3078c11b23319a97607f871b0ed336cc4fb9dee1
kenchen680807 <jinwei0917@yahoo.com>
2016-07-05 20:04:02 +0800

    HW2

---
+++ HW2_iphone6/js/index.js
@@ -0,0 +1,9 @@
+$( ".flip" ).click(function() {
+  $( '.iPhone' ).toggleClass( "rotate" );
+  $( '.usman' ).toggleClass( "rotateBack" );
+  $( '.usman img' ).toggleClass( "center" );
+});
+$( ".circButton" ).click(function() {
+  $( '.iScreen' ).toggleClass( "turnOn" );
+  $( '.usman' ).toggleClass( "showMe" );
+});
