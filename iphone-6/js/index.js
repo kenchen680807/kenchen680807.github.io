fa2741aad91a8d95f46731a6f14f8b104a084ac6
kenchen680807 <jinwei0917@yahoo.com>
2016-07-05 20:02:27 +0800

    HW1

--- iphone-6/js/index.js
+++
@@ -1,9 +0,0 @@
-$( ".flip" ).click(function() {
-  $( '.iPhone' ).toggleClass( "rotate" );
-  $( '.usman' ).toggleClass( "rotateBack" );
-  $( '.usman img' ).toggleClass( "center" );
-});
-$( ".circButton" ).click(function() {
-  $( '.iScreen' ).toggleClass( "turnOn" );
-  $( '.usman' ).toggleClass( "showMe" );
-});
--- phptest/config.php
+++
@@ -1,9 +0,0 @@
-<html>
-
-<head>
-    <title>layout</title>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
-<?php include_once("dbclass/db.php");?>
